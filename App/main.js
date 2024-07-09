c660571a3eb6dbb5789602a03566b95d119aeb3b
Seojin <selwyngadioma@gmail.com>
2024-07-09 08:33:56 +0800

    Added variables at js file and modified some styles

---
+++ App/main.js
@@ -0,0 +1,17 @@
+// Visual variables
+const backgroundColor = document.body.style 
+
+// Displayer variables
+const headTitle = document.getElementById('head-title');
+const timerTitle = document.getElementById('main-header');
+const timerDisplay = document.getElementById('time-display');
+
+// Timer Selection variables
+const typeSession = document.getElementById('session-button');
+const typeRest = document.getElementById('rest-button');
+const typeBreak = document.getElementById('break-button');
+
+// Button variables
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const stopButton = document.getElementById('stop-button');
