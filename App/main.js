96118e5e63151cc4a38d193128535b9fd060ebbb
Seojin <selwyngadioma@gmail.com>
2024-07-09 08:48:28 +0800

    Added session button function

--- App/main.js
+++ App/main.js
@@ -15,3 +15,33 @@ const typeBreak = document.getElementById('break-button');
 const playButton = document.getElementById('play-button');
 const pauseButton = document.getElementById('pause-button');
 const stopButton = document.getElementById('stop-button');
+
+// Timer types object
+const sessionTimer = {
+    type: "session",
+    accentColor: "var(--clr-red)",
+    header: "Session",
+}
+
+// Conditional variables
+let timerType = 'session';
+
+typeSession.onclick = () => {
+
+    if(timerType!=='session') {
+
+        console.log("Switching to session timer...");
+        timerType = sessionTimer.type;
+
+        // Visual change
+        timerTitle = sessionTimer.header;
+        backgroundColor = sessionTimer.accentColor;
+        typeSession.style.backgroundColor = 'var(--clr-black)';
+        typeSession.style.color = sessionTimer.accentColor;
+    }
+
+    else {
+
+        console.log("You're already at session timer.");
+    }
+}
