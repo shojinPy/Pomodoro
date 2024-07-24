c5c896b2c1e2743b57de70e1b7dd418095375485
Seojin <selwyngadioma@gmail.com>
2024-07-24 11:08:10 +0800

    Added logic function to timer

--- App/main.js
+++ App/main.js
@@ -21,64 +21,96 @@ const selectType = {
     sessionTimer: {
         type: "Session",
         accentColor: "var(--clr-red)",
-        timerValue: 25,
+        timerValue: 1500000,
     },
 
     restTimer: {
         type: "Rest",
         accentColor: "var(--clr-orange)",
-        timerValue: 5,
+        timerValue: 300000,
     },
 
     breakTimer: {
         type: "Break",
         accentColor: "var(--clr-green)",
-        timerValue: 30,
+        timerValue: 1800000,
     },
 }
 
 // Conditional variables
 let timerType = 'session';
 let isRunning = false;
+let confirmExit = false;
+let timerSpan = selectType.sessionTimer.timerValue; // 1 500 000 miliseconds
+timerDisplay.textContent = `${timerSpan / (1000 * 60)}:00`;
 
 // Timer variables
-let timerSpan = selectType.sessionTimer.timerValue;
-let playTime = null;
-let pauseTime = null;
-
-function startTimer(timer) {
+let startTime = Date.now() + timerSpan; // Output = 1 500 000 ms
+let timeLeft = timerSpan;
+let timerInterval = null;
 
 
+// Functions
+function playTimer() {
+    timeLeft = startTime - Date.now(); // Decreased the time (Constantly running)
+
+    if(timeLeft < 0){
+
+        clearInterval(timerInterval);
+        timerDisplay.textContent = "TIME'S UP";
+        alert(`${timerSpan / (1000 * 60)} minutes is over!`);
+        return;
+    }
+
+    const minutesLeft = Math.floor(timeLeft / (1000 * 60)) % 60;
+    const secondsLeft = Math.round(timeLeft / 1000)% 60;
+
+    console.log(`${minutesLeft} : ${secondsLeft}`);
+    timerDisplay.textContent = `${minutesLeft}:${singleDigitHandler(secondsLeft)}`;
+}
+
+function singleDigitHandler(number) {
+
+    if(String(number).length<=1) {
+        return `0${number}`;
+    }
+    else{
+        return number;
+    }
 }
 
 // Timer button function
 playButton.onclick = () => {
     
     if(!isRunning) {
+        console.log("Timer is now running!");
+
         // Visual
         playButton.style.display = 'none';
         stopButton.style.display = 'none';
         pauseButton.style.display = 'block';
 
         // Logic
-        console.log(timerSpan);
+        startTime = Date.now() + timeLeft;
+        timerInterval = setInterval(playTimer, 1000);
 
         isRunning = true;
-    }
-
-    else {
-
     }
 }
 
 pauseButton.onclick = () => {
     
     if(isRunning) {
+        console.log("Timer paused!");
 
         playButton.style.display = 'block';
         stopButton.style.display = 'block';
         pauseButton.style.display = 'none';
 
+        // Logic
+        clearInterval(timerInterval);
+        timeLeft = startTime - Date.now();
+        
         isRunning = false;
     }
 
@@ -89,13 +121,26 @@ pauseButton.onclick = () => {
 
 stopButton.onclick = () => {
 
+    if(timerSpan!=timeLeft || timeLeft==undefined){
+        if(confirm("Are you sure to reset the timer?")==true){
+            startTime = Date.now() + timerSpan;
+            timeLeft = timerSpan;
+            timerInterval = null;
+
+            console.log("Timer has been reset.");
+        }
+    }
 }
 
 
 // Select timer type function
 typeSession.onclick = () => {
 
-    if(timerType!='session') {
+    if(isRunning===true) {
+        alert("Can't switch mode while running!");
+    }
+
+    else if(timerType!='session') {
 
         console.log("Switching to session timer...");
         timerType = selectType.sessionTimer.type.toLowerCase();
@@ -106,6 +151,11 @@ typeSession.onclick = () => {
         documentBody.style.backgroundColor = selectType.sessionTimer.accentColor;
         typeSession.style.color = selectType.sessionTimer.accentColor;
         typeSession.style.backgroundColor = 'var(--clr-black)';
+
+        // Re-declare variables
+        timerDisplay.textContent = `${timerSpan / (1000 * 60)}:00`;
+        startTime = Date.now() + timerSpan; // Output = 1 500 000 ms
+        timeLeft = timerSpan;
 
         playButton.style.display = 'block';
         stopButton.style.display = 'block';
@@ -127,7 +177,11 @@ typeSession.onclick = () => {
 
 typeRest.onclick = () => {
 
-    if(timerType!='rest') {
+    if(isRunning===true) {
+        alert("Can't switch mode while running!");
+    }
+
+    else if(timerType!='rest') {
 
         console.log("Switching to rest timer...");
         timerType = selectType.restTimer.type.toLowerCase();
@@ -138,6 +192,12 @@ typeRest.onclick = () => {
         documentBody.style.backgroundColor = selectType.restTimer.accentColor;
         typeRest.style.color = selectType.restTimer.accentColor;
         typeRest.style.backgroundColor = 'var(--clr-black)';
+
+        // Re-declare variables
+        timerDisplay.textContent = `${timerSpan / (1000 * 60)}:00`;
+        startTime = Date.now() + timerSpan; // Output = 1 500 000 ms
+        timeLeft = timerSpan;
+
 
         playButton.style.display = 'block';
         stopButton.style.display = 'block';
@@ -159,7 +219,11 @@ typeRest.onclick = () => {
 
 typeBreak.onclick = () => {
 
-    if(timerType!='break') {
+    if(isRunning===true) {
+        alert("Can't switch mode while running!");
+    }
+
+    else if(timerType!='break') {
 
         console.log("Switching to break timer...");
         timerType = selectType.breakTimer.type.toLowerCase();
@@ -170,6 +234,11 @@ typeBreak.onclick = () => {
         documentBody.style.backgroundColor = selectType.breakTimer.accentColor;
         typeBreak.style.backgroundColor = 'var(--clr-black)';
         typeBreak.style.color = selectType.breakTimer.accentColor;
+
+        // Re-declare variables
+        timerDisplay.textContent = `${timerSpan / (1000 * 60)}:00`;
+        startTime = Date.now() + timerSpan; // Output = 1 500 000 ms
+        timeLeft = timerSpan;
 
         playButton.style.display = 'block';
         stopButton.style.display = 'block';
